2e32364f1ceb8ffaf3c999383fbc89f200fee194
Alex Delasalle <alex_delasalle@aol.co.uk>
2023-02-28 13:02:49 +0000

    Database sorted

--- db/migrate/20230228030247_drop_superheros.rb
+++
@@ -1,5 +0,0 @@
-class DropSuperheros < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :superheros, force: :cascade
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'date'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+#Users
+puts 'destroying all the users..'
+puts '-----------------------'
+puts '-----------------------'
+puts '-----------------------'
+
+puts 'creating new users....'
+5.times do
+  User.create(email: Faker::Internet.email, password: Faker::Internet.password)
+end
+puts "Created #{User.count} users"
+
+#Superheros
+puts 'destroying all the superheros..'
+puts '-----------------------'
+puts '-----------------------'
+puts '-----------------------'
+
+puts 'creating new superheros....'
+5.times do
+  Superhero.create(email: Faker::Internet.email, password: Faker::Internet.password)
+end
+puts "Created #{Superhero.count} superheros"
+
+#Bookings
+puts 'destroying all the bookings..'
+puts '-----------------------'
+puts '-----------------------'
+puts '-----------------------'
+
+puts 'creating new bookings....'
+10.times do
+  date = Date.today + rand(20)
+  Booking.create!(start_date: date, end_date: date + rand(5))
+end
+puts "Created #{Booking.count} bookings"
